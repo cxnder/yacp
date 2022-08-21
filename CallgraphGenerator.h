b258496b5fe851bf1e2f3fced019028dab3f2d04
kat <katherine@vector35.com>
2022-08-21 04:56:33 -0400

    Basically rewrite the plugin
    Multithreading node generation
    
    Support for scoping
    
    Optionally embed HLIL in nodes
    
    Background all analysis
    
    Huge speed improvements

--- CallgraphGenerator.h
+++ CallgraphGenerator.h
@@ -6,11 +6,25 @@
 #define YACP_CALLGRAPHGENERATOR_H
 
 #include "binaryninjaapi.h"
+#include "binja/ui/progresstask.h"
 #include <map>
+#include <QString>
+#include <utility>
 
 
 using namespace std;
 using namespace BinaryNinja;
+
+
+struct CallGraphSettings {
+    string name = string("");
+    bool generateUpwards = false;
+    bool generateDownwards = false;
+    bool generateFull = false;
+    bool embedHLIL = false;
+    int64_t upwardScope = -1;
+    int64_t downwardScope = -1;
+};
 
 
 struct CallGraphNode
@@ -24,15 +38,22 @@ struct CallGraphNode
     Ref<FlowGraphNode> node;
 };
 
+class CallgraphGenerator;
+
+static map<Ref<BinaryView>, CallgraphGenerator*> callGraphInstances;
+static map<CallgraphGenerator*, ProgressTask*> tasks;
 
 class CallgraphGenerator : public BinaryDataNotification
 {
+    Ref<Function> m_currentTarget;
 
     Ref<BinaryView> m_data;
     Ref<Logger> m_logger;
 
     map<Ref<Function>, CallGraphNode*> m_nodeMap;
     vector<CallGraphNode*> m_nodes;
+
+    std::atomic<int> m_done;
 
     bool m_cacheValid;
 
@@ -42,15 +63,25 @@ public:
 
     CallgraphGenerator(Ref<BinaryView> data);
 
+    bool ValidateAlive() { return m_data.GetPtr() != nullptr; };
+
+    void SetCurrentTarget(Ref<Function> func) { m_currentTarget = std::move(func); };
+
+    Ref<BinaryView> GetView() { return m_data; };
+
     void OnAnalysisFunctionAdded(BinaryView *view, Function *func) override;
     void OnAnalysisFunctionUpdated(BinaryView *view, Function *func) override;
     void OnAnalysisFunctionRemoved(BinaryView *view, Function *func) override;
 
     void RebuildCache();
 
-    Ref<FlowGraph> GenerateCallgraphInDirection(Ref<Function> func, bool up, bool down, bool full = false);
+    Ref<FlowGraph> GenerateCallgraph(CallGraphSettings settings);
+
+    ProgressTask* CurrentTask() { return tasks.count(this) > 0 ? tasks[this] : nullptr; };
+
+    void TrySetTaskTextAndProgress(std::string text, size_t done, size_t jobs);
+
 };
 
-static map<Ref<BinaryView>, CallgraphGenerator*> callGraphInstances;
 
 #endif //YACP_CALLGRAPHGENERATOR_H
